8dd3a3f2aceec4317d5bdae98f8d0043ceba1ee1
Homy Chen <homychen@gmail.com>
2018-09-19 16:09:54 -0700

    Added index.js and index.html templates

---
+++ index.js
@@ -0,0 +1,55 @@
+const exp = require("express");
+const port = process.env.PORT || 10000;
+const path = require("path");
+const bodyParser = require("body-parser");
+
+//require session
+//const session = require("express-session");
+
+var pF = path.resolve(__dirname, "public");
+var app = exp();
+
+//create a new server for socket, but combine it with express functions
+const server = require("http").createServer(app);
+
+//create a socket server with the new server
+var io = require("socket.io")(server);
+
+//postgres
+const pg = require("pg");
+
+//dburl
+var dbURL = process.env.DATABASE_URL || "postgres://postgres:password@localhost:0000/databasename";
+
+app.use("/scripts", exp.static("build"));
+app.use("/css", exp.static("style"));
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+
+//use sessions
+/*app.use(session({
+    secret:"",
+    resave:true,
+    saveUninitialized:true
+}));
+*/
+
+/*app.get("/", function(req, resp){
+    if(req.session.user){
+        resp.sendFile(pF+"/item.html")
+    }else{
+        resp.sendFile(pF+"/login.html");
+    }
+});
+*/
+
+server.listen(10000, function(err){
+    if(err){
+        console.log(err);
+        return false;
+    }
+    
+    console.log(port+" is running");
+});
