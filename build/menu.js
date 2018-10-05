6505b9eec75edb59d409e82626d9ce8ea1dd2248
slee814 <sorlui0701@hotmail.com>
2018-10-04 19:17:09 -0700

    update menu

--- build/menu.js
+++ build/menu.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
 
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
+        $('#sidebarCollapse').toggleClass('dactive');
+        $('#sidebarCollapse').removeClass("glyphicon glyphicon-menu-hamburger");
+
+
     });
 
 });
+
+//$('#sidebarCollapse').toggleClass('glyphicon glyphicon-remove');
