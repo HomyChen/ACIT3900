e04bbafa38482687dd4224f9ad787dc97a479a26
slee814 <sorlui0701@hotmail.com>
2018-10-05 13:49:08 -0700

    fixed menu issue

--- build/menu.js
+++ build/menu.js
@@ -3,11 +3,10 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('#sidebarCollapse').toggleClass('dactive');
-        $('#sidebarCollapse').removeClass("glyphicon glyphicon-menu-hamburger");
-
-
+       
     });
 
 });
 
-//$('#sidebarCollapse').toggleClass('glyphicon glyphicon-remove');
+
+
