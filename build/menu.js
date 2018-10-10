94301a4c7522156e6c99a4ca6537ef41bddcb985
Homy Chen <homychen@gmail.com>
2018-10-10 16:42:17 -0700

    updated menu?

--- build/menu.js
+++ build/menu.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('#sidebarCollapse').toggleClass('dactive');
+        $('#burgericon').toggleClass('glyphicon glyphicon-remove-circle');
     });
 
 });
