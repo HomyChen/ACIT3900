e078bcab2cec7b2f2c074cb2aea32e6bae45b9b5
Homy Chen <homychen@gmail.com>
2018-10-12 14:11:59 -0700

    merged jap's insert functions

--- index.js
+++ index.js
@@ -1,52 +1,42 @@
+/**
+ * ToDo: Package data from other requests and send to DB, need to set up promises for data
+ */
+
 const exp = require("express");
 const port = process.env.PORT || 10000;
 const path = require("path");
 const bodyParser = require("body-parser");
 
-//require session
-//const session = require("express-session");
+// had to change button Id of second add button for the other request option
 
+var dbFunctions = require("./routes/dbFunctions");
 var pF = path.resolve(__dirname, "public");
 var app = exp();
 
 //create a new server for socket, but combine it with express functions
 const server = require("http").createServer(app);
 
-//create a socket server with the new server
-var io = require("socket.io")(server);
 
-//postgres
-const pg = require("pg");
-
-//dburl
-var dbURL = process.env.DATABASE_URL || "postgres://postgres:password@localhost:11000/databasename";
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/scripts", exp.static("build"));
-
 app.use("/css", exp.static("style"));
+app.use("/pages",exp.static("public"))
+
 app.use(bodyParser.urlencoded({
     extended:true
 }));
+
 app.get("/", function(req, resp){
     resp.sendFile(pF+"/checkin.html")
 });
+app.use("/data",dbFunctions);
 
-//use sessions
-/*app.use(session({
-    secret:"",
-    resave:true,
-    saveUninitialized:true
-}));
-*/
-
-/*app.get("/", function(req, resp){
-    if(req.session.user){
-        resp.sendFile(pF+"/item.html")
-    }else{
-        resp.sendFile(pF+"/login.html");
-    }
-});
-*/
 
 server.listen(10000, function(err){
     if(err){
@@ -60,8 +50,4 @@ server.listen(10000, function(err){
 
 app.get("/menu", function(req, resp){
     resp.sendFile(pF+"/menu.html")
-});
-
-app.get("/orders", function(req, resp){
-    resp.sendFile(pF+"/ro.html")
 });
