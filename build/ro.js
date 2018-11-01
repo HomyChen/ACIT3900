0c6c9d610863b48024716c359c6cc8f6bd071214
slee814 <sorlui0701@hotmail.com>
2018-11-01 16:19:15 -0700

    repair order fully viewable now

--- build/ro.js
+++ build/ro.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
     // buttons
     var searchROBut = document.getElementById("searchROBut");
     var popupClose = document.getElementById("popupClose");
+    var editRO = document.getElementById("editRO");
+    var saveRO = document.getElementById("saveRO");
     
     // repair order popup
     var roPopup = document.getElementById("roPopup");
@@ -28,6 +30,15 @@ $(document).ready(function() {
     var roYear = document.getElementById("roYear");
     var roMake = document.getElementById("roMake");
     var roModel = document.getElementById("roModel");
+    var roOdometerIn = document.getElementById("roOdometerIn");
+    var roOdometerOut = document.getElementById("roOdometerOut");
+    var roNotes = document.getElementById("roNotes");
+    
+    var roTask = document.getElementById("roTask");
+    var odometerOut= document.getElementById("odometerOut");
+    var promisedTime = document.getElementById("promisedTime");
+    
+    var roDetails = document.getElementById("roDetails");
     
     searchROBut.onclick = function() {
     console.log("search button clicked");
@@ -45,9 +56,6 @@ $(document).ready(function() {
             success:function(data){
                 if (data){
                     console.log(data);
-                
-                    
-                var roDetails = document.getElementById("roDetails");
                     
                 var resultsTable = $('#resultsTable').DataTable({
                     destroy: true,
@@ -64,12 +72,13 @@ $(document).ready(function() {
                     
                 });
                  
-                
                 resultsTable.on('select', function ( e, dt, type, indexes ) {
                     var rowData = resultsTable.rows( indexes ).data()[0];
-                    console.log(rowData);
-                    //roDetails.innerHTML="";
-                    //roDetails.innerHTML="Repair Order Details'<br>" + rowData.first_name + "<br>"
+
+                    odometerOut.disabled = true;
+                    promisedTime.disabled = true;
+                    saveRO.className = "btn btn-default pull-right invisible";
+                    editRO.className = "btn btn-default pull-right visible";
                     
                     roPopup.style.display = "block";
                     roNum.innerHTML = rowData.ro_id;
@@ -81,14 +90,108 @@ $(document).ready(function() {
                     roMake.innerHTML = rowData.make;
                     roModel.innerHTML = rowData.model;
                     roYear.innerHTML = rowData.year;
+                    roOdometerIn.innerHTML = rowData.odometer_in;
+                    //roOdometerOut.innerHTML = "<input class='form-control input-sm' id='OdometerOut' disabled>"
+                        //rowData.odometer_out;
+                    roNotes.innerHTML = rowData.vehicle_notes;
+                    
+                    $.ajax({
+                        url:"/rosearch/taskSearch",
+                        type:"post",
+                        data:{
+                            roID:rowData.ro_id
+                        },
+                        success:function(data){
+                        if (data){
+                            console.log(data);
+                            
+                            roTask.innerHTML="";
+                            for(var i = 0; i<data.length; i++){
+                                var comment = data[i].comments;
+                                var box = '\u2610';
+                                var linebreak = document.createElement('br');
+                                var horLine = document.createElement('HR');
+                                horLine.className = 'taskLine';
+                                var taskName = data[i].task_name; 
+                                var taskEntry = document.createElement('li');
+                                var editTask = document.createElement("textarea");
+                                editTask.className = 'form-control';
+                                editTask.id = 'worktask' + data[i].worktask_id;
+                                editTask.disabled = true;
+                                editTask.rows = '5';
+                                editTask.style.marginBottom = '10px';
+                                
+                                
+                                if(comment == null){
+                                    editTask.value = "";
+                                }else{
+                                    editTask.value = comment;
+                                }
+                                
+                                taskEntry.appendChild(document.createTextNode(taskName)).appendChild;
+
+                                roTask.appendChild(taskEntry);
+                                roTask.appendChild(editTask);
+                                //roTask.appendChild(linebreak);
+                                //roTask.appendChild(horLine);
+                                //roTask.appendChild(linebreak.cloneNode());
+                                
+                                //roTask.appendChild(horLine.cloneNode());
+                 
+                            }
+                            
+                            editRO.onclick = function(){
+                                for(var j = 0; j<data.length; j++){
+                                    document.getElementById('worktask' + data[j].worktask_id).disabled = false;
+                                }
+                                    saveRO.className = "btn btn-default pull-right visible";
+                                    editRO.className = "btn btn-default pull-right invisible";
+                                    odometerOut.disabled = false;
+                                    promisedTime.disabled = false;
+                                
+                            }
+                            
+                            /*
+                            var taskComments = {
+                                ('worktask' + data[k].worktask_id) : ('worktask' + data[k].worktask_id).value,
+                                ('worktask' + data[k].worktask_id) : ('worktask' + data[k].worktask_id).value
+                            }
+                            */    
+                            saveRO.onclick = function(){
+                                    saveRO.className = "btn btn-default pull-right invisible";
+                                    editRO.className = "btn btn-default pull-right visible";
+                                    odometerOut.disabled = true;
+                                    promisedTime.disabled = true;
+                                for(var k = 0; k<data.length; k++){
+                                    document.getElementById('worktask' + data[k].worktask_id).disabled = true;
+                                }
+                                /*
+                                 $.ajax({
+                                    url:"/rosearch/updateRO",
+                                    type:"post",
+                                    data:{
+                                        roID:rowData.ro_id,
+                                        
+                                    },
+                                    success:function(data){
+                                        if (data){
+                                            console.log(data);
+                                        }
+                                    }
+                                 });
+                                */
+                            }                        
+                            
+                            }
+
+                        }
+                    });   
                 });
                 
-            
-                
-                    
+                  
                 }
                 else{
-                    alert("Error!");
+                    alert("Error! taskSearch");
                 }
             }
         });
@@ -96,13 +199,17 @@ $(document).ready(function() {
     }
     
     
+
+    
     popupClose.onclick = function() {
         roPopup.style.display = "none";
+        roTask.innerHTML="";
     }
     
     window.onclick = function(event) {
         if (event.target == roContainer) {
             roPopup.style.display = "none";
+            roTask.innerHTML="";
     }
 }
     
--- routes/roFunctions.js
+++ routes/roFunctions.js
@@ -31,9 +31,21 @@ router.post("/AroSearch", function (req,resp){
     console.log(searchBy);
     console.log(roStatus);
     
-    var roQuery = 'SELECT * FROM customer c INNER JOIN vehicle v ON c.cust_id = v.cust_id INNER JOIN repair_order ro ON ro.vehicle_id = v.vehicle_id where ' + searchBy + '= $1';
+    if(searchBy == "ro_id"){
+        var roQuery = 'SELECT * FROM customer c INNER JOIN vehicle v ON c.cust_id = v.cust_id INNER JOIN repair_order ro ON ro.vehicle_id = v.vehicle_id where ro.' + searchBy + ' =$1' + ' AND status=$2';
+        
+        var data = [roSearchWord, roStatus];
+        
+    }else{
+        var roQuery = 'SELECT * FROM customer c INNER JOIN vehicle v ON c.cust_id = v.cust_id INNER JOIN repair_order ro ON ro.vehicle_id = v.vehicle_id where ' + searchBy + ' LIKE $1' + ' AND status=$2';
+        
+        var data = [roSearchWord + '%', roStatus];
+    }
     
-    var data = [req.body.roSearchWord];
+    
+    
+    console.log(roQuery);
+    console.log(data);
     
     pool.connect(function (err, client, done){
         if (err) {
@@ -77,5 +89,38 @@ router.post("/AroSearch", function (req,resp){
     })
 });
 
+
+router.post("/taskSearch", function (req,resp){
+    console.log("taskSearch ajax");
+    
+    var roID = req.body.roID;
+    
+    console.log(roID);
+    
+    var data = [roID];
+    
+    var taskQuery = 'SELECT task_name, comments, worktask_id FROM repair_tasks rt INNER JOIN task t ON rt.task_id = t.task_id WHERE ro_id = $1';
+    
+    pool.connect(function (err, client, done){
+        if (err) {
+            console.log("(taskSearch - Unable to connect to the database: " + err );
+        }
+        else{
+            console.log("taskSearch - Successfully login to database!")
+        }
+        
+        client.query(taskQuery, data, function(err, result){
+            done();
+            if(err){
+                console.log(err.message);
+                resp.send(null);
+            }
+            else{
+                console.log(result.rows);
+                resp.send(result.rows);
+            }
+        })
+    })
+});
 
 module.exports = router;
